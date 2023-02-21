8d094ee1d52c3774e033a5a6f708b81e07b142cd
kamarini <kama0549@stud.kea.dk>
2023-02-21 19:17:05 +0100

    sort toggles direction

--- animalbase.js
+++ animalbase.js
@@ -55,25 +55,39 @@ function filterClick(event){
 function sortClick(event){
 
     const sortBy = event.target.dataset.sort;
-    console.log(`user selected ${sortBy}`)
-    sortList(sortBy)
+    const sortDir = event.target.dataset.sortDirection;
+
+    //toggle the direction
+    if(sortDir === "asc"){
+        event.target.dataset.sortDirection = "desc";
+    }else{
+        event.target.dataset.sortDirection = "asc";
+    }
+    console.log(`user selected ${sortBy} - ${sortDir}`)
+    sortList(sortBy, sortDir)
     
     
 }
 
 
 
-function sortList(sortBy){
+function sortList(sortBy , sortDir){
      let sortedList = allAnimals;
+     let direction = 1;
+     if(sortDir === "desc"){
+        direction = -1;
+     } else {
+        direction = 1;
+     }
     
      sortedList = sortedList.sort(sortByInput);
     
       function sortByInput(animalA, animalB){
         console.log(`sorted by ${sortBy}`)
         if(animalA[sortBy]   < animalB[sortBy]){
-            return -1;
+            return -1 * direction;
         }else{
-            return 1;
+            return 1 * direction;
         }
     }
      displayList(sortedList);
