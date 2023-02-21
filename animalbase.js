8c63746a1ab769ec8e51e01a0189da62b8ea2e1b
kamarini <kama0549@stud.kea.dk>
2023-02-21 21:16:21 +0100

    end of the day

--- animalbase.js
+++ animalbase.js
@@ -75,6 +75,14 @@ function sortClick(event){
     const sortBy = event.target.dataset.sort;
     const sortDir = event.target.dataset.sortDirection;
 
+    //find "old" sortby element and remove .sortBy
+    const oldElement = document.querySelector(`[data-sort=${settings.sortBy}]`);
+    oldElement.classList.remove("sortby");
+
+
+    //indicate active sort
+    event.target.classList.add("sortby");
+
     //toggle the direction
     if(sortDir === "asc"){
         event.target.dataset.sortDirection = "desc";
